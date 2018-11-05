1aa754870a97aa57524ddea659c1ade7f4672484
nikhil_karkera <v-nikhil@elitmus.com>
2018-11-05 11:11:06 +0530

    changed css of the editor and the button

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     console.log('message: ' + msg);
-//   });
-// });
-
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
